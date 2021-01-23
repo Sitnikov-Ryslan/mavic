9803b76e0b784064c4630942d54da12cef5e6195
Sitnikov-Ryslan <63663502+Sitnikov-Ryslan@users.noreply.github.com>
2021-01-23 20:09:39 +0400

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,8 @@ $(function(){
         nextArrow: '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>'
     });
 
-
+    $('.questions__item-title').on('click', function() {
+        $('.questions__item').removeClass('questions__item_active');
+        $(this).parent().addClass('questions__item_active');
+    })
 });
